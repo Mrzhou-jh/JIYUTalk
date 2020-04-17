4a9cf05e01f60b7982ee50a606f0aa5878fc7fa0
MrZhou-jh <211535176@qq.com>
2020-04-17 17:54:09 +0800

    profile组件搭建

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -7,38 +7,47 @@
     </head-nav-bar>
     <ul>
       <li>
-        <h4>头像</h4><span></span>
+        <h4>头像</h4>
+        <span></span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>签名</h4><span>沙河事实上</span>
+        <h4>签名</h4>
+        <span>沙河事实上</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>注册</h4><span></span>20171217
+        <h4>注册</h4>
+        <span></span>20171217
       </li>
       <li>
-        <h4>昵称</h4><span>你个哈哈</span>
+        <h4>昵称</h4>
+        <span>你个哈哈</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>性别</h4><span>男</span>
+        <h4>性别</h4>
+        <span>男</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>生日</h4><span>19930330</span>
+        <h4>生日</h4>
+        <span>19930330</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>邮箱</h4><span>123456@qq.com</span>
+        <h4>邮箱</h4>
+        <span>123456@qq.com</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>电话</h4><span>1234567891</span>
+        <h4>电话</h4>
+        <span>1234567891</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
       <li>
-        <h4>密码</h4><span>*************</span>
+        <h4>密码</h4>
+        <span>*************</span>
         <svg-icon slot="nav-bar-left" iconname="arrowright"></svg-icon>
       </li>
     </ul>
@@ -49,6 +58,11 @@
 export default {};
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+ul {
+  padding-left: 16px;
+  li {
+    list-style: none;
+  }
+}
 </style>
